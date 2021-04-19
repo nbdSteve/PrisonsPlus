dff2c3d5190c8871be956d156291f21928afe8ad
Steve Goodhill <s.goodhill@protonmail.com>
2021-04-19 19:02:16 +1000

    [Rebase] Rebased repo to start from scratch

--- src/main/java/gg/steve/mc/pp/tokens/event/AddMethodType.java
+++
@@ -1,7 +0,0 @@
-package gg.steve.mc.pp.tokens.event;
-
-public enum AddMethodType {
-    COMMAND,
-    MINE,
-    TOKENATOR;
-}
--- src/main/java/gg/steve/mc/pp/tokens/player/TokenType.java
+++
@@ -1,6 +0,0 @@
-package gg.steve.mc.pp.tokens.player;
-
-public enum TokenType {
-    TOKEN,
-    PRESTIGE;
-}
